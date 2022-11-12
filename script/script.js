b1b75713a5b3db8fe7e65da8faace6c4ce90cc96
Ruan Figuereido <huanfiguereido@gmail.com>
2022-11-12 17:30:38 -0300

    adição de eps e tentativa de mudar src

--- script/script.js
+++ script/script.js
@@ -13,3 +13,18 @@ player contruido de forma diferente*/
 function frame2(){
     window.open('html/iframeplayer.html');
 }
+/*NOVA OPÇAO COM SWITCH CASE
+aqui abaixo ao clicar no episodio dispara uma função que entra em
+acao apos 3 segundos atribuindo um src ao video*/
+/*let play=document.getElementsByClassName('play');
+let video=document.getElementById('video');
+function frame2(){
+    window.open('html/iframeplayer.html');
+    setTimeout(function a(){
+        video.src;
+    switch(video.src){
+        case play[1] : video.src='https://youtube.com/onepiece/';break;
+    }
+},3000);
+}*/
+
