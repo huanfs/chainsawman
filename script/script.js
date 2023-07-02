34be085a6caad2d5e006a7201139a81693204a2a
Ruan Figuereido <huanfiguereido@gmail.com>
2023-07-02 11:19:41 -0300

    identação do código

--- script/script.js
+++ script/script.js
@@ -1,15 +1,9 @@
 let ref=[];
 let player = document.querySelector('iframe');
-
-
-
-
-
-
-
-
-
-
+//se houver algo dentro do array, executa esta função para
+// salvar no local storage o número do episodio, abrir o iframePlayer
+// obtem o valor de local storage e recarrega a página para limpar o
+// array ref
 function processamento(referencia){
     if(ref.length!=0){
         localStorage.setItem('ref',ref);
@@ -18,16 +12,8 @@ function processamento(referencia){
         location.reload();
     }
 }
-
-
-
-
-
-
-
-
-
-
+//Função que atribui o valor de local storage para abrir o episodio
+// correspondente, então limpa o valor de local sotrage
 function episodio(){
     if(localStorage.getItem('ref')==1){
         player.src='D:/chainsawman ep1.mp4';
@@ -63,6 +49,18 @@ function episodio(){
     }
     else if(localStorage.getItem('ref')==9){
         player.src='D:/chainsawman ep9.mp4';
+        localStorage.removeItem('ref');
+    }
+    else if(localStorage.getItem('ref')==10){
+        player.src='D:/chainsawman ep10.mp4';
+        localStorage.removeItem('ref');
+    }
+    else if(localStorage.getItem('ref')==11){
+        player.src='D:/chainsawman ep11.mp4';
+        localStorage.removeItem('ref');
+    }
+    else if(localStorage.getItem('ref')==12){
+        player.src='D:/chainsawman ep12.mp4';
         localStorage.removeItem('ref');
     }
     //so ir adicionando as condições para que seja carregado o episodio
