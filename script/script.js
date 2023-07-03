4ca150a1b069737b9d7be5952394d48f94b58ba3
Ruan Figuereido <huanfiguereido@gmail.com>
2023-07-03 18:51:22 -0300

    mudanças de como o código funciona nesta branch, código alternativo

--- script/script.js
+++ script/script.js
@@ -1,68 +1,69 @@
-let ref=[];
 let player = document.querySelector('iframe');
-//se houver algo dentro do array, executa esta função para
-// salvar no local storage o número do episodio, abrir o iframePlayer
-// obtem o valor de local storage e recarrega a página para limpar o
-// array ref
-function processamento(referencia){
-    if(ref.length!=0){
-        localStorage.setItem('ref',ref);
-        window.open('html/iframeplayer.html');
-        const refFinal = localStorage.getItem('ref')
-        location.reload();
-    }
+let ep = document.querySelectorAll("article");
+
+for(let i of ep){
+    i.addEventListener("click", Processamento);
 }
-//Função que atribui o valor de local storage para abrir o episodio
-// correspondente, então limpa o valor de local sotrage
-function episodio(){
-    if(localStorage.getItem('ref')==1){
-        player.src='D:/chainsawman ep1.mp4';
-        localStorage.removeItem('ref');
-    }
-    else if(localStorage.getItem('ref')==2){
-        player.src='D:/chainsawman ep2.mp4';
-        localStorage.removeItem('ref');
-    }
-    else if(localStorage.getItem('ref')==3){
-        player.src='D:/chainsawman ep3.mp4';
-        localStorage.removeItem('ref');
-    }
-    else if(localStorage.getItem('ref')==4){
-        player.src='D:/chainsawman ep4.mp4';
-        localStorage.removeItem('ref');
-    }
-    else if(localStorage.getItem('ref')==5){
-        player.src='D:/chainsawman ep5.mp4';
-        localStorage.removeItem('ref');
-    }
-    else if(localStorage.getItem('ref')==6){
-        player.src='D:/chainsawman ep6.mp4';
-        localStorage.removeItem('ref');
-    }
-    else if(localStorage.getItem('ref')==7){
-        player.src='D:/chainsawman ep7.mp4';
-        localStorage.removeItem('ref');
-    }
-    else if(localStorage.getItem('ref')==8){
-        player.src='D:/chainsawman ep8.mp4';
-        localStorage.removeItem('ref');
-    }
-    else if(localStorage.getItem('ref')==9){
-        player.src='D:/chainsawman ep9.mp4';
-        localStorage.removeItem('ref');
-    }
-    else if(localStorage.getItem('ref')==10){
-        player.src='D:/chainsawman ep10.mp4';
-        localStorage.removeItem('ref');
-    }
-    else if(localStorage.getItem('ref')==11){
-        player.src='D:/chainsawman ep11.mp4';
-        localStorage.removeItem('ref');
-    }
-    else if(localStorage.getItem('ref')==12){
-        player.src='D:/chainsawman ep12.mp4';
-        localStorage.removeItem('ref');
-    }
-    //so ir adicionando as condições para que seja carregado o episodio
+
+function Processamento(key){
+let arr = [];
+arr.push(key.target.alt);
+if(arr.includes('episodio 01')){
+    localStorage.setItem("ep",arr[0]);
+    window.open("html/iframeplayer.html","blank");
 }
+else if(arr.includes('episodio 02')){
+    localStorage.setItem("ep",arr[0]);
+    window.open("html/iframeplayer.html","blank");
+}
+else if(arr.includes('episodio 03')){
+    localStorage.setItem("ep",arr[0]);
+    window.open("html/iframeplayer.html","blank");
+}
+else if(arr.includes('episodio 04')){
+    localStorage.setItem("ep",arr[0]);
+    window.open("html/iframeplayer.html","blank");
+}
+else if(arr.includes('episodio 05')){
+    localStorage.setItem("ep",arr[0]);
+    window.open("html/iframeplayer.html","blank");
+}
+else if(arr.includes('episodio 06')){
+    localStorage.setItem("ep",arr[0]);
+    window.open("html/iframeplayer.html","blank");
+}
+else if(arr.includes('episodio 07')){
+    localStorage.setItem("ep",arr[0]);
+    window.open("html/iframeplayer.html","blank");
+}
+else if(arr.includes('episodio 08')){
+    localStorage.setItem("ep",arr[0]);
+    window.open("html/iframeplayer.html","blank");
+}
+else if(arr.includes('episodio 09')){
+    localStorage.setItem("ep",arr[0]);
+    window.open("html/iframeplayer.html","blank");
+}
+else if(arr.includes('episodio 10')){
+    localStorage.setItem("ep",arr[0]);
+    window.open("html/iframeplayer.html","blank");
+}
+else if(arr.includes('episodio 11')){
+    localStorage.setItem("ep",arr[0]);
+    window.open("html/iframeplayer.html","blank");
+}
+else if(arr.includes('episodio 12')){
+    localStorage.setItem("ep",arr[0]);
+    window.open("html/iframeplayer.html","blank");
+}
+}
+
+
+function Carregamento(){
+    let num = localStorage.getItem("ep");
+    let player = document.querySelector("iframe");
+    player.src="https://youtube.com/q761572";//atribuir os SRC de cada episodio
+    //que será o valor obtido de local storage ex:"nome do episodio + local storage + mp4";
+}
+
 
