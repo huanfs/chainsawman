1fd9907a4162e9f0ed5e3816cf3a0589a6662fe7
Ruan Figuereido <huanfiguereido@gmail.com>
2023-07-04 18:33:05 -0300

    criação da tela e funcionalidade de login

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 let player = document.querySelector('iframe');
 let ep = document.querySelectorAll("article");
-
+let wid = window.innerWidth;
 for(let i of ep){
     i.addEventListener("click", Processamento);
 }
@@ -10,51 +10,51 @@ let arr = [];
 arr.push(key.target.alt);
 if(arr.includes('episodio01')){
     localStorage.setItem("ep",arr[0]);
-    window.open("html/iframeplayer.html","blank");
+    window.open("../html/iframeplayer.html","blank");
 }
 else if(arr.includes('episodio02')){
     localStorage.setItem("ep",arr[0]);
-    window.open("html/iframeplayer.html","blank");
+    window.open("../html/iframeplayer.html","blank");
 }
 else if(arr.includes('episodio03')){
     localStorage.setItem("ep",arr[0]);
-    window.open("html/iframeplayer.html","blank");
+    window.open("../html/iframeplayer.html","blank");
 }
 else if(arr.includes('episodio04')){
     localStorage.setItem("ep",arr[0]);
-    window.open("html/iframeplayer.html","blank");
+    window.open("../html/iframeplayer.html","blank");
 }
 else if(arr.includes('episodio05')){
     localStorage.setItem("ep",arr[0]);
-    window.open("html/iframeplayer.html","blank");
+    window.open("../html/iframeplayer.html","blank");
 }
 else if(arr.includes('episodio06')){
     localStorage.setItem("ep",arr[0]);
-    window.open("html/iframeplayer.html","blank");
+    window.open("../html/iframeplayer.html","blank");
 }
 else if(arr.includes('episodio07')){
     localStorage.setItem("ep",arr[0]);
-    window.open("html/iframeplayer.html","blank");
+    window.open("../html/iframeplayer.html","blank");
 }
 else if(arr.includes('episodio08')){
     localStorage.setItem("ep",arr[0]);
-    window.open("html/iframeplayer.html","blank");
+    window.open("../html/iframeplayer.html","blank");
 }
 else if(arr.includes('episodio09')){
     localStorage.setItem("ep",arr[0]);
-    window.open("html/iframeplayer.html","blank");
+    window.open("../html/iframeplayer.html","blank");
 }
 else if(arr.includes('episodio10')){
     localStorage.setItem("ep",arr[0]);
-    window.open("html/iframeplayer.html","blank");
+    window.open("../html/iframeplayer.html","blank");
 }
 else if(arr.includes('episodio11')){
     localStorage.setItem("ep",arr[0]);
-    window.open("html/iframeplayer.html","blank");
+    window.open("../html/iframeplayer.html","blank");
 }
 else if(arr.includes('episodio12')){
     localStorage.setItem("ep",arr[0]);
-    window.open("html/iframeplayer.html","blank");
+    window.open("../html/iframeplayer.html","blank");
 }
 }
 
@@ -66,3 +66,43 @@ function Carregamento(){
 }
 
 
+function log(){
+    let wallpapper = document.querySelectorAll("img")[0];
+    let register = document.querySelectorAll("main aside")[0];
+    if(wid>600){
+        register.style.marginLeft="-200px";
+        wallpapper.style.marginLeft="100px";
+        setTimeout(x=>{
+            wallpapper.src="D:/denji2.jpg";
+            window.open("./html/app.html");
+        },1000);  
+    }
+    else if(wid<=600){
+        register.style.marginTop="-250px";
+        wallpapper.style.marginTop="100px";
+        setTimeout(x=>{
+            wallpapper.src="D:/denji2.jpg";
+            window.open("./html/app.html");
+        },1000);
+    }
+}
+
+function exit(){
+    window.close();
+}
+
+function foco(){
+    let wallpapper = document.querySelectorAll("img")[0];
+    let register = document.querySelectorAll("main aside")[0];
+    if(wid>600){
+        wallpapper.style.marginLeft="0px";
+        register.style.marginLeft="0px";
+        register.style.opacity="1";
+        wallpapper.src="D:/denji4.jpg"
+    }
+    else if(wid<=600){
+        wallpapper.style.marginTop="0px";
+        register.style.marginTop="0px";
+        wallpapper.src="D:/denji4.jpg"
+    }
+}
