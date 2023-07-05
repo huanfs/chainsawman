2466e2ed2c6a837686c5901509af3d83594654fa
Ruan Figuereido <huanfiguereido@gmail.com>
2023-07-05 16:31:33 -0300

    finalização da primeira etapa. tudo está funcionando, faltando apenas salvar os videos no servidor remoto para que possam ser acessados de outra origem e adicionar o papel de parede dinamico

--- script/script.js
+++ script/script.js
@@ -1,10 +1,12 @@
-let player = document.querySelector('iframe');
+//*****TODOS OS ELEMENTOS ARTICLE QUE SERÃO CLICAVEIS*****
 let ep = document.querySelectorAll("article");
+//*****VARIÁVEL QUE ARMAZENA A LARGURA DA TELA******
 let wid = window.innerWidth;
+//*****LAÇO DE REPETIÇÃO QUE APLICA O EVENTO CLICK A TODOS OS EP(ARTICLE);
 for(let i of ep){
     i.addEventListener("click", Processamento);
 }
-
+//*****FUNÇÃO QUE ARMAZENA O ATRIBUTO ALT DA IMAGEM DENTRO DE ARTICLE NO LOCAL STORAGE E ENTÃO A RECUPERA PARA ATRIBUIR AO EPISODIO CORRESPONDENTE*****
 function Processamento(key){
 let arr = [];
 arr.push(key.target.alt);
@@ -57,50 +59,59 @@ else if(arr.includes('episodio12')){
     window.open("../html/iframeplayer.html","blank");
 }
 }
-
-
+//*****FUNÇÃO ATIVADA QUANDO A PÁGINA DO PLAYER CARREGA PARA SETAR O SRC DE LOCAL STORAGE A TAG VIDEO*****
 function Carregamento(){
     let num = localStorage.getItem("ep");
     let video = document.getElementById("video");
     video.src=`file://D:/chainsawman_${num}.mp4`;
 }
-
-
+//******FUNÇÃO ATIVADA NO BOTÃO "ENTRAR COMO CONVIDADO" QUE ESCONDE A ÁREA DE LOGIN E ABRE APP.HTML*****
 function log(){
+    let arrow = document.getElementById("arrow");
+    arrow.addEventListener("click", show);
     let wallpapper = document.querySelectorAll("img")[0];
     let register = document.querySelectorAll("main aside")[0];
     if(wid>600){
         register.style.marginLeft="-200px";
         wallpapper.style.marginLeft="100px";
+        arrow.src="./img/arrow-right.png";
         setTimeout(x=>{
             wallpapper.src="./img/wp2.jpg";
             window.open("./html/app.html");
+            arrow.style.display="block";
         },1000);  
     }
     else if(wid<=600){
-        register.style.marginTop="-250px";
+        register.style.marginTop="-200px";
         wallpapper.style.marginTop="100px";
+        arrow.src="./img/arrow-down.png";
         setTimeout(x=>{
             wallpapper.src="./img/wp2.jpg";
             window.open("./html/app.html");
+            arrow.style.display="block";
         },1000);
     }
 }
+//*****FUNÇÃO QUE ABRE A ÁREA DE LOGIN OCULTADA ANTERIORMENTE*****
+function show(){
+    let arrow = document.getElementById("arrow");
+    if(wid>600&&arrow.style.display=="block"){
+        let wallpapper = document.querySelectorAll("img")[0];
+        let register = document.querySelectorAll("main aside")[0];
+        register.style.marginLeft="0";
+        wallpapper.style.marginLeft="0";
+        arrow.style.display="none";
+    }
+    else if(wid<=600&&arrow.style.display=="block"){
+        let wallpapper = document.querySelectorAll("img")[0];
+        let register = document.querySelectorAll("main aside")[0];
+        register.style.marginTop="0";
+        wallpapper.style.marginTop="0";
+        arrow.style.display="none";
+    }
+}
+//*****FUNÇÃO QUE SAI DE APP.HTML E VOLTA PARA INDEX.HTML*****
 function exit(){
     window.close();
 }
-
-function foco(){
-    let wallpapper = document.querySelectorAll("img")[0];
-    let register = document.querySelectorAll("main aside")[0];
-    if(wid>600){
-        wallpapper.style.marginLeft="0px";
-        register.style.marginLeft="0px";
-        wallpapper.src="./img/wp1.jpg";
-    }
-    else if(wid<=600){
-        wallpapper.style.marginTop="0px";
-        register.style.marginTop="0px";
-        wallpapper.src="./img/wp1.jpg";
-    }
-}
+//criar uma função de papel de parede dinamico
