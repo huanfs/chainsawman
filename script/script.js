20a653192c60832a7ff4377601e98a9dd2a101d4
Ruan Figuereido <huanfiguereido@gmail.com>
2023-07-09 19:04:51 -0300

    organização do código e conclusão da função register/login. onde register salva usuario e senha em dois campos separados de local storage e login os analisa separadamente

--- script/script.js
+++ script/script.js
@@ -2,8 +2,6 @@
 let ep = document.querySelectorAll("article");
 //*****VARIÁVEL QUE ARMAZENA A LARGURA DA TELA******
 let wid = window.innerWidth;
-//*****ONDE AS INFORMAÇÕES DE USUÁRIO SÃO GRAVADAS******
-let usuario = [];
 //*****LAÇO DE REPETIÇÃO QUE APLICA O EVENTO CLICK A TODOS OS EP(ARTICLE);
 for(let i of ep){
     i.addEventListener("click", Processamento);
@@ -14,64 +12,64 @@ let arr = [];
 arr.push(key.target.alt);
 if(arr.includes('episodio01')){
     localStorage.setItem("ep",arr[0]);
-    window.open("../html/iframeplayer.html","blank");
+        window.open("../html/iframeplayer.html","blank");
 }
 else if(arr.includes('episodio02')){
     localStorage.setItem("ep",arr[0]);
-    window.open("../html/iframeplayer.html","blank");
+        window.open("../html/iframeplayer.html","blank");
 }
 else if(arr.includes('episodio03')){
     localStorage.setItem("ep",arr[0]);
-    window.open("../html/iframeplayer.html","blank");
+        window.open("../html/iframeplayer.html","blank");
 }
 else if(arr.includes('episodio04')){
     localStorage.setItem("ep",arr[0]);
-    window.open("../html/iframeplayer.html","blank");
+        window.open("../html/iframeplayer.html","blank");
 }
 else if(arr.includes('episodio05')){
     localStorage.setItem("ep",arr[0]);
-    window.open("../html/iframeplayer.html","blank");
+        window.open("../html/iframeplayer.html","blank");
 }
 else if(arr.includes('episodio06')){
     localStorage.setItem("ep",arr[0]);
-    window.open("../html/iframeplayer.html","blank");
+        window.open("../html/iframeplayer.html","blank");
 }
 else if(arr.includes('episodio07')){
     localStorage.setItem("ep",arr[0]);
-    window.open("../html/iframeplayer.html","blank");
+        window.open("../html/iframeplayer.html","blank");
 }
 else if(arr.includes('episodio08')){
     localStorage.setItem("ep",arr[0]);
-    window.open("../html/iframeplayer.html","blank");
+        window.open("../html/iframeplayer.html","blank");
 }
 else if(arr.includes('episodio09')){
     localStorage.setItem("ep",arr[0]);
-    window.open("../html/iframeplayer.html","blank");
+        window.open("../html/iframeplayer.html","blank");
 }
 else if(arr.includes('episodio10')){
     localStorage.setItem("ep",arr[0]);
-    window.open("../html/iframeplayer.html","blank");
+        window.open("../html/iframeplayer.html","blank");
 }
 else if(arr.includes('episodio11')){
     localStorage.setItem("ep",arr[0]);
-    window.open("../html/iframeplayer.html","blank");
+        window.open("../html/iframeplayer.html","blank");
 }
 else if(arr.includes('episodio12')){
     localStorage.setItem("ep",arr[0]);
-    window.open("../html/iframeplayer.html","blank");
+        window.open("../html/iframeplayer.html","blank");
 }
 }
 //*****FUNÇÃO ATIVADA QUANDO A PÁGINA DO PLAYER CARREGA PARA SETAR O SRC DE LOCAL STORAGE A TAG VIDEO*****
 function Carregamento(){
     let num = localStorage.getItem("ep");
     let video = document.getElementById("video");
-    // video.src=`file://D:/chainsawman_${num}.mp4`;
+    // video.src=`file://D:/chainsawman_${num}.mp4`; //ACESSO LOCAL DIRETO
     video.src=`http://localhost/vid/chainsawman_${num}.mp4`;//ACESSO A LOCAL HOST
 }
 //******FUNÇÃO ATIVADA NO BOTÃO "ENTRAR COMO CONVIDADO" QUE ESCONDE A ÁREA DE LOGIN E ABRE APP.HTML*****
 function log(){
     let arrow = document.getElementById("arrow");
-    arrow.addEventListener("click", show);
+        arrow.addEventListener("click", show);
     let wallpapper = document.querySelectorAll("img")[0];
     let register = document.querySelectorAll("main aside")[0];
     if(wid>600){
@@ -101,16 +99,16 @@ function show(){
     if(wid>600&&arrow.style.display=="block"){
         let wallpapper = document.querySelectorAll("img")[0];
         let register = document.querySelectorAll("main aside")[0];
-        register.style.marginLeft="0";
-        wallpapper.style.marginLeft="0";
-        arrow.style.display="none";
+            register.style.marginLeft="0";
+            wallpapper.style.marginLeft="0";
+            arrow.style.display="none";
     }
     else if(wid<=600&&arrow.style.display=="block"){
         let wallpapper = document.querySelectorAll("img")[0];
         let register = document.querySelectorAll("main aside")[0];
-        register.style.marginTop="0";
-        wallpapper.style.marginTop="0";
-        arrow.style.display="none";
+            register.style.marginTop="0";
+            wallpapper.style.marginTop="0";
+            arrow.style.display="none";
     }
 }
 //*****FUNÇÃO QUE SAI DE APP.HTML E VOLTA PARA INDEX.HTML*****
@@ -121,8 +119,8 @@ function exit(){
 function enterOrRegister(){
     let enter = document.querySelectorAll("article button")[0];
     let register = document.querySelectorAll("article button")[1];
-    enter.addEventListener("click", signIn);
-    register.addEventListener("click", Register);
+        enter.addEventListener("click", signIn);
+        register.addEventListener("click", Register);
     //*****FUNÇÃO DE ENTRAR QUE CRIA TODOS OS CAMPOS DA ABA ENTRAR
     function signIn(){
         let field = document.querySelector("aside");
@@ -130,27 +128,27 @@ function enterOrRegister(){
             field.removeChild(field.firstChild);
         }
         let user = document.createElement("input");
-        user.setAttribute("placeholder","insira seu usuário");
-        user.type="text";
+            user.setAttribute("placeholder","insira seu usuário");
+                user.type="text";
         let password = document.createElement("input");
-        password.setAttribute("placeholder","insira sua senha");
-        password.type="password";
+            password.setAttribute("placeholder","insira sua senha");
+                password.type="password";
         let entrar = document.createElement("input");
-        entrar.type="button";
-        entrar.value="entrar";
-        entrar.style.backgroundColor="#ff0202d7";
-        entrar.style.color="#fff";
-        entrar.addEventListener("click", enter);
+            entrar.type="button";
+                entrar.value="entrar";
+                    entrar.style.backgroundColor="#ff0202d7";
+                        entrar.style.color="#fff";
+                            entrar.addEventListener("click", enter);
         field.style.gap="1em";
-        field.appendChild(user);
-        field.appendChild(password);
-        field.appendChild(entrar);
+            field.appendChild(user);
+                field.appendChild(password);
+                    field.appendChild(entrar);
         //*****FUNÇÃO QUE VALIDA A ENTRADA, CHECANDO SE USUARIO E SENHA FORAM PREVIAMENTE SALVOS EM LOCAL STORAGE*****
         function enter(){
             let user = document.querySelectorAll("input")[0];
             let password = document.querySelectorAll("input")[1];
-            let credenciais = localStorage.getItem("usuario");
-            if(user.value!=credenciais[0]||password.value!=credenciais[1]){
+            //let credenciais = localStorage("usuario");
+            if(user.value!=localStorage.userName||password.value!=localStorage.userPassword){
                 user.value='';
                 password.value='';
                 if(confirm("usuário não encontrado! deseja registrar-se?")){
@@ -159,7 +157,7 @@ function enterOrRegister(){
                     location.reload();
                 }
             }
-            else if(user.value==credenciais[0]&&password.value==credenciais[1]){
+            else if(user.value==localStorage.userName&&password.value==localStorage.userPassword){
                 log();
             }
         }
@@ -171,39 +169,34 @@ function enterOrRegister(){
             field.removeChild(field.firstChild);
         }
         let user = document.createElement("input");
-        user.setAttribute("placeholder","insira seu usuário");
-        user.type="text";
-        user.id="user";
+            user.setAttribute("placeholder","insira seu usuário");
+                user.type="text";
+                    user.id="user";
         let password = document.createElement("input");
-        password.setAttribute("placeholder","insira sua senha");
-        password.type="text";
-        password.id="password";
+            password.setAttribute("placeholder","insira sua senha");
+                password.type="text";
+                    password.id="password";
         let confirmPassword = document.createElement("input");
-        confirmPassword.setAttribute("placeholder","confirme sua senha");
-        confirmPassword.type="text";
-        confirmPassword.id="cPassword";
+            confirmPassword.setAttribute("placeholder","confirme sua senha");
+                confirmPassword.type="text";
+                    confirmPassword.id="cPassword";
         let registrar = document.createElement("input");
-        registrar.type="button";
-        registrar.value="registrar";
-        registrar.style.backgroundColor="#ff0202d7";
-        registrar.style.color="#fff";
-        registrar.addEventListener("click", passwordCheck);
+            registrar.type="button";
+                registrar.value="registrar";
+                    registrar.style.backgroundColor="#ff0202d7";
+                        registrar.style.color="#fff";
+                            registrar.addEventListener("click", passwordCheck);
         field.style.gap="1em";
-        field.appendChild(user);
-        field.appendChild(password);
-        field.appendChild(confirmPassword);
-        field.appendChild(registrar);
-        //LOCAL STORAGE ESTÁ SALVANDO OS DADOS EM FORMA DE STRING POR EXTENSO
-        //AO ACESSAR USUARIO[0] RETORNA A PRIMEIRA LETRA. TENHO QUE CORRIGIR ISTO
-        //FUNÇÃO DE REGISTRAR QUE SALVA USUARIO E SENHA EM LOCAL STORAGE
-        //PRECISO USAR JSON.parse() para converter uma string json em objeto e 
-        //assim recuperar os valores pra fazer a checagem da função logar
+            field.appendChild(user);
+                field.appendChild(password);
+                    field.appendChild(confirmPassword);
+                        field.appendChild(registrar);
+        //*****FUNÇÃO DE REGISTRAR QUE SALVA USUARIO E SENHA EM LOCAL STORAGE*****
         function register(){
             let userId = document.querySelectorAll("input")[0].value;
             let userPassword = document.querySelectorAll("input")[2].value;
-            usuario.push(userId);
-            usuario.push(userPassword);
-            localStorage.setItem("usuario",usuario);
+                localStorage.setItem("userName", userId);
+                localStorage.setItem("userPassword", userPassword);
             log();
         }
         //*****FUNÇÃO QUE CHECA OS DADOS INSERIDOS NO REGISTRO*****
@@ -211,7 +204,7 @@ function enterOrRegister(){
         if(user.value.length<6){
             alert("O usuário deve ter no mínimo, 6 caracteres");
             user.style.border="2px solid red";
-            user.value='';
+                user.value='';
         }
         if(password.value.length<7){
             alert("Sua senha deve ter no mínimo 8 caracteres");
@@ -229,6 +222,5 @@ function enterOrRegister(){
             register();
         }
     }
-    }
-    
+    }  
 }
